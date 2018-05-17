e8b4341f8bca3d34b80c01c6afa7e0ff5b95bd2d
Per-Olof Bondesson - Vendor <pbondesson@walmartlabs.com>
2018-05-17 14:14:28 -0700

    chat example

---
+++ signalr/Startup.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace example_websit
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSignalR()
+                    .AddAzureSignalR("Endpoint=https://burning-analytics.service.signalr.net;AccessKey=Rt4sI9aL5Wmfcik9PvwB8G0xxrs8Mt6SS0UJVDUVu+w=;");
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<Chat>("/chat");
+            });
+        }
+    }
+}
